c6accbdc40cfe7f1a51203f1f3c27a74008610be
andrewijaya <wijayabagusandre@gmail.com>
2022-08-25 14:44:22 +0700

    change serves

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const database = require('./connectDb');
 const app = require('./index')
 const port = 3000;
 const server = http.createServer(app);
-server.listen(port)
-console.log('Server running at http://127.0.0.1:3000/');
+server.listen(process.env.PORT || 3000, () => {
+    console.log(`Server started on port`);
+});
