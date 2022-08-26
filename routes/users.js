82582622e90948ee2f237bf9962906c55836e336
andrewijaya <wijayabagusandre@gmail.com>
2022-08-26 19:05:48 +0700

    add post users

--- routes/users.js
+++ routes/users.js
@@ -7,14 +7,14 @@ const router = express.Router();
 router.get('/login', (req, res) => {
 var account = req.query;
 var accountbody = req.body;
-var query = "select * from users where username = ? and password = ?";
-database.query(query,[account.username,account.password],(err,rows)=>{
+var query = "select * from users where email = ? and password = ?";
+database.query(query,[account.email,account.password],(err,rows)=>{
     if (!err) {
         if (rows.length == 0 ) {
-            database.query(query,[accountbody.username,accountbody.password],(err,rows)=>{
+            database.query(query,[accountbody.email,accountbody.password],(err,rows)=>{
                 if (!err) {
                     if (rows.length == 0 ) {
-                        return res.status(200).json({status:"false",response:"Unknow username or Password"});
+                        return res.status(200).json({status:"false",response:"Unknow email or Password"});
                     }else{
                         
                         return res.status(200).json({status:"true",rows});
@@ -29,5 +29,23 @@ database.query(query,[account.username,account.password],(err,rows)=>{
         return res.status(500).json(err);
     }
 })
+});
+
+
+router.post('/register', (req, res) => {
+    var dataparams = req.query;
+    var insert = "insert into users(email,username,password,role) values(?,?,?,2)";
+
+    database.query(insert,[dataparams.email,dataparams.username,dataparams.password],(err,result)=>
+    {
+        if (!err) {
+            return res.status(200).json({status:"true",response:"Succesfully Registered!"});
+        }else{
+            return res.status(200).json({status:"false",response:"Email Hasbeen Registered"});
+        }
+      
+    }
+    )
+
 });
 module.exports = router;
